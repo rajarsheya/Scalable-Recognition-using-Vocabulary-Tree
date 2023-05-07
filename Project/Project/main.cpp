5a840119239091adb4f78d66110606f3e5b2d739
Sugam Jaiswal <67289887+jaissugam@users.noreply.github.com>
2023-05-06 19:26:45 -0700

    empty project

---
+++ Project/Project/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgcodecs.hpp>
+
+using namespace cv;
+				
+int main(int argc, char* argv[])
+{	
+		Mat image = imread("pippy.jpg");
+		
+		namedWindow("Original Image");
+		imshow("Original Image", image);
+		waitKey(0);
+
+		return 0;
+}
