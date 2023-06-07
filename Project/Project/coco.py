97fa30a36e7ac7110da2caec49f5390b1b0e1166
Sugam Jaiswal <67289887+jaissugam@users.noreply.github.com>
2023-06-07 14:11:49 -0700

    python for coco

--- Project/Project/coco.py
+++ Project/Project/coco.py
@@ -2,14 +2,15 @@ import requests
 from pycocotools.coco import COCO
 
 # Load the annotations
-coco = COCO('./instances_val2017.json')
+#coco = COCO('./annotations_val2017.json')
+coco = COCO('./annotations_new_train2017.json')
 
 # Get the image ids and urls
 imgIds = coco.getImgIds()
 images = coco.loadImgs(imgIds)
 
 # Save the images into a local folder
-for image in images:
+for image in images[5500:]:
   data = requests.get(image['coco_url']).content
-  with open('./coco/' + image['file_name'], 'wb') as handler:
+  with open('./coco118k/' + image['file_name'], 'wb') as handler:
     handler.write(data)
